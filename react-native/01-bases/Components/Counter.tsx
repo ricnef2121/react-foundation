271079eb665ddf3f46fb7eaa836bbdd0f89c3a2f
ricnef2121 <ricnef2121@gmail.com>
2026-09-16 03:55:33 -0600

    feat: add counter functionality

--- react-native/01-bases/Components/Counter.tsx
+++ react-native/01-bases/Components/Counter.tsx
@@ -2,14 +2,24 @@ import { useState } from "react"
 
 export const Counter = () => {
 
-    const [count, setCount] = useState(10)
+    const [count, setCount] = useState<number>(10)
+
+    // avoid get or achive negative numbers 
+    const increaseBy = (value: number)=>{
+        setCount(Math.max(value + count, 0))
+    }
 
     return (
         <>
-            <h3>Contador: <small>{count}</small></h3>
+            <h3>Contador: <small className="font-bold">{count}</small></h3>
             <div>
-                <button className="p-2 bg-blue-500 rounded-xl w-10 mx-2 text-white" onClick={() => setCount(count + 1)}>
+                <button 
+                className="p-2 bg-blue-500 rounded-xl w-10 mx-2 text-white hover:bg-blue-700" onClick={() => increaseBy(+1)}>
                     +1
+                </button>
+                <button 
+                className="p-2 bg-blue-500 rounded-xl w-10 mx-2 text-white hover:bg-blue-700" onClick={() => increaseBy(-1)}>
+                    -1
                 </button>
             </div>
         </>
